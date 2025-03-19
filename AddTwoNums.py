d9365b2ad4ea95246990cb6e797477ee81138e71
unknown <1607629453@qq.com>
2025-03-19 11:19:21 +0800

    AddTwoNums V3

--- AddTwoNums.py
+++ AddTwoNums.py
@@ -1,7 +1,11 @@
 #Add two numbers in python
 #Author:Zheng Zhongxing
-#Using user input
+#Using function
+def add(a,b):
+   result=a+b
+   return result
 a=float(input("First Number:"))
 b=float(input("Second Number:"))
 res=a+b
+print("The answer is:")
 print(res)
